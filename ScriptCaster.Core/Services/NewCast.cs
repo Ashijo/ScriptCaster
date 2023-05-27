ab46ef4b701cf61e8961689d5c55aa65506a6be4
Ashijo <gpasquet412@gmail.com>
2023-05-27 12:06:38 -0400

    fluid parse on path

--- ScriptCaster.Core/Services/NewCast.cs
+++ ScriptCaster.Core/Services/NewCast.cs
@@ -29,7 +29,7 @@ public static partial class Cast
 		var templateFolders = GetAllFolders();
 		var filesInTemplate = GetAllFiles(templateFolders);
 
-		CreateFolders(fluidModel, templateFolders);
+		CreateFoldersNew(context, fluidModel, templateFolders);
 		CreateFilesNew(context, filesInTemplate, force);
 
 		return new RLaunchCastCallback(true, true);
@@ -57,6 +57,27 @@ public static partial class Cast
 		return new RValidateVariablesCallback(true, true, true, variables);
 	}
 
+	
+	private static void CreateFoldersNew(TemplateContext fluidContext, Dictionary<string, string> variables, string[] templatesFolders)
+	{
+		var parser = new FluidParser();
+
+		foreach (var templateFolder in templatesFolders)
+		{
+			Debug.Assert(Context.TemplatePath != null, "TemplatePath is null in Cast.CreateFolders");
+			
+			var resultFolder = FluidParse(fluidContext, parser, templateFolder)
+				.Replace("\n", "");
+
+			for (var i = 0; i < Context.RecursionLevel; i++)
+			{
+				resultFolder = FluidParse(fluidContext, parser, templateFolder);
+			}
+
+			Directory.CreateDirectory(resultFolder);
+		}
+	}
+	
 	private static void CreateFilesNew(TemplateContext fluidContext, string[] filesInTemplate,
 		bool forced = false)
 	{
@@ -65,40 +86,57 @@ public static partial class Cast
 		foreach (var tFilePath in filesInTemplate)
 		{
 			Debug.Assert(Context.TemplatePath != null, "TemplatePath is null in Cast.LaunchCast");
-			var resultFilePath = tFilePath
-				.Replace(Context.TemplatePath, Context.LocalPath)
-				.Replace("\n", "");
 
-			var resultFileExist = File.Exists(resultFilePath);
+			var newFilePath = FluidParse(fluidContext, parser, tFilePath);
+			
+			for (var i = 0; i < Context.RecursionLevel - 1; i++) 
+			{
+				newFilePath = FluidParse(fluidContext, parser, tFilePath);
+			}
+
+			//TODO: verify if this is mandatory, I think yes but...
+			newFilePath = newFilePath.Replace("\n", "");
+			
+			var resultFileExist = File.Exists(newFilePath);
 
 			if (resultFileExist && !forced)
 			{
 				if (!forced)
 				{
 					Console.WriteLine(
-						$"{resultFilePath} already exist. Ignored. You can force replace with -f or --force.");
+						$"{newFilePath} already exist. Ignored. You can force replace with -f or --force.");
 					continue;
 				}
 			}
 
 			if (resultFileExist)
 			{
-				Directory.Delete(resultFilePath);
+				Directory.Delete(newFilePath);
 			}
 
 			var fileContent = File.ReadAllText(tFilePath);
 			
-			for (var i = 0; i < Context.RecursionLevel; i++)
+			for (var i = 0; i < Context.RecursionLevel - 1; i++)
 			{
-				if (parser.TryParse(fileContent, out var template, out var error))
-				{
-					fileContent = template.Render(fluidContext);
-				}
+				fileContent = FluidParse(fluidContext, parser, fileContent);
 			}
 
-			using var stream = File.CreateText(resultFilePath);
+			using var stream = File.CreateText(newFilePath);
 			stream.Write(fileContent);
 			stream.Close();
 		}
+	}
+
+	private static string FluidParse(TemplateContext fluidContext, FluidParser parser, string fileContent)
+	{
+		string? result = null;
+		if (parser.TryParse(fileContent, out var fluidResult, out var error))
+		{
+			result = fluidResult.Render(fluidContext);
+		}
+		
+		//TODO: manage exceptions
+		
+		return result ?? "";
 	}
 }
